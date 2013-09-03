fa048f06421d1bd5a53725dca7137d628770267b
Jay Wengrow <jaywngrw@gmail.com>
2013-09-03 12:25:07 -0500

    added TODO

--- thesaurus.rb
+++ thesaurus.rb
@@ -25,9 +25,10 @@ end
 
 # Possible things TODO:
 # 1. Remove entry from Thesaurus
-# 2. When creating new entries, create entries based off the entries with appropriate synonyms
-# 3. Adding and removing synonyms/antonyms from entry
-# 4. Better API for initializing Thesaurus. Possible example:
+# 2. Create a clean lookup method(s) that looks up synonyms/antonyms in the thesaurus
+# 3. When creating new entries, create entries based off the entries with appropriate synonyms
+# 4. Adding and removing synonyms/antonyms from entry
+# 5. Better API for initializing Thesaurus. Possible example:
 #    t = Thesaurus.new do
 #      Entry.new("happy") do
 #        synonyms ["glad", "content"]
