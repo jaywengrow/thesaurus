b6913b2f86d87ea961e46b94e38e057ed4384c58
Jay Wengrow <jaywngrw@gmail.com>
2013-09-17 12:10:11 -0500

    exercises 1 though 4

--- thesaurus.rb
+++ thesaurus.rb
@@ -9,6 +9,30 @@ class Thesaurus
   def <<(entry)
     @entries.merge!({entry.word => entry})
   end
+
+  def create_mirror_entries(word)
+    synonyms(word).each do |synonym|
+      if exists?(synonym)
+        @entries[synonym].add_synonym(word) if !synonyms(synonym).include?(word)
+      else #doesn't exist
+        self << Entry.new(synonym, {:synonyms => ([word] + synonyms(word) - [synonym])})
+        # create_mirror_entries(synonym)
+      end
+    end
+  end
+
+  def delete_entry(word)
+    @entries[word] = nil if @entries[word]
+  end
+
+  def synonyms(word)
+    @entries[word].synonyms
+  end
+
+  def exists?(word)
+    !@entries[word].nil?
+  end
+
 end
 
 class Entry
@@ -17,9 +41,15 @@ class Entry
 
   def initialize(word, args={})
     @word = word
-    @synonyms = args[:synonyms]
-    @antonyms = args[:antonyms]
+    @synonyms = args[:synonyms] || []
+    @antonyms = args[:antonyms] || []
   end
+
+  def add_synonym(word)
+    @synonyms << word
+  end
+
+
 
 end
 
@@ -27,7 +57,7 @@ end
 # 1. Add method to Thesaurus that removes entries from the Thesaurus
 # 2. Add methods to Entry that add more synonyms/antonyms to an existing Entry.
 # 3. As of now, the only way to look up synonyms in the thesaurus is thesaurus.entries["happy"].synonyms. Create a cleaner lookup method(s) that looks up synonyms/antonyms in the thesaurus.
-# 4. When creating new entries, create mirror entries based on synonyms. For example, if we add to a thesaurus an Entry for "happy" that has a synonym for "glad", that should automatically create an entry for "glad" that has a synonym "happy".
+# 4. When creating new entries, create mirror entries based on synonyms. For example, if we add to a thesaurus an Entry for "happy" that has a synonym "glad", that should automatically create an entry for "glad" that has a synonym "happy".
 # 5. Develop a cleaner API for initializing Thesaurus. Possible example:
 #    t = Thesaurus.new do
 #      Entry.new("happy") do
--- thesaurus_test.rb
+++ thesaurus_test.rb
@@ -6,7 +6,63 @@ class ThesaurusTest < MiniTest::Unit::TestCase
   def test_add_entry
     thesaurus = Thesaurus.new
     thesaurus << Entry.new("happy")
-    assert_equal(thesaurus.entries["happy"].class, Entry)
+    assert_equal(Entry, thesaurus.entries["happy"].class)
+  end
+
+  def test_delete_entry
+    thesaurus = Thesaurus.new
+    thesaurus << Entry.new("happy")
+    thesaurus.delete_entry("happy")
+    assert_equal(nil, thesaurus.entries["happy"])
+  end
+
+  def test_synonyms
+    thesaurus = Thesaurus.new
+    entry = Entry.new("happy", :synonyms => ["glad"])
+    thesaurus << entry
+    assert_equal(entry.synonyms, thesaurus.synonyms("happy"))
+  end
+
+  def test_exists?
+    thesaurus = Thesaurus.new
+    entry = Entry.new("happy", :synonyms => ["glad"])
+    assert_equal(false, thesaurus.exists?("happy"))
+  end
+
+  def test_create_mirror_entries
+    thesaurus = Thesaurus.new
+    entry = Entry.new("happy", {:synonyms => ["glad"]})
+    thesaurus << entry
+    thesaurus.create_mirror_entries("happy")
+    assert_equal(["happy"], thesaurus.synonyms("glad"))
+  end
+
+  def test_creating_mirror_entry_with_multiple_synonyms
+    thesaurus = Thesaurus.new
+    entry = Entry.new("happy", {:synonyms => ["glad", "gleeful"]})
+    thesaurus << entry
+    thesaurus.create_mirror_entries("happy")
+    assert_equal(["happy", "gleeful"], thesaurus.synonyms("glad"))
+  end
+
+  def test_creating_mirror_entry_with_existing
+    thesaurus = Thesaurus.new
+    entry = Entry.new("happy", {:synonyms => ["glad"]})
+    entry2 = Entry.new("glad", {:synonyms => ["gleeful"]})
+    thesaurus << entry
+    thesaurus << entry2
+    thesaurus.create_mirror_entries("happy")
+    assert_equal(["gleeful", "happy"], thesaurus.synonyms("glad"))
+  end
+
+end
+
+class EntryTest < MiniTest::Unit::TestCase
+
+  def test_add_synonym
+    entry = Entry.new("apartments", {:synonyms => ["condominium"]})
+    entry.add_synonym("house")
+    assert_equal(entry.synonyms.last, "house")
   end
 
 end
